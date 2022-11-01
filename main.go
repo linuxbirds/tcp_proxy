5e07b5abfc61a0df049936350deddffc6f7f847c
chin <411239339@qq.com>
2022-11-01 10:59:50 +0800

    change flag to ini

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 
 	"github.com/fatih/color"
+	"github.com/go-ini/ini"
 )
 
 var red = color.New(color.FgRed).SprintFunc()
@@ -16,17 +16,34 @@ var blue = color.New(color.FgBlue).SprintFunc()
 func main() {
 
 	// Parse the starting parameters
-	fromAddress := flag.String("from", "127.0.0.1:8888", blue("From which address to proxied"))
-	toAddress := flag.String("to", "127.0.0.1:8080", blue("To which address to proxied"))
+	//fromAddress := flag.String("from", "127.0.0.1:8888", blue("From which address to proxied"))
+	//toAddress := flag.String("to", "127.0.0.1:8080", blue("To which address to proxied"))
+	//
+	//flag.Parse()
 
-	flag.Parse()
+	cfgs, err := ini.Load("tcpproxy.ini")
+	if err != nil {
+		fmt.Println("Ini format err:", err)
+		return
+	}
 
-	fromListener, err := net.Listen("tcp", *fromAddress)
+	section := cfgs.Section("TcpProxy")
+	fromAddress := section.Key("fromAddress").Value()
+	toAddress := section.Key("toAddress").Value()
+
+	fromListener, err := net.Listen("tcp", fromAddress)
 
 	if err != nil {
-		log.Fatal(red("Unable to listen on: %s, error : %s\n", *fromAddress, err.Error()))
+		log.Fatal(red("Unable to listen on: %s, error : %s\n", fromAddress, err.Error()))
+		return
 	}
-	defer fromListener.Close()
+
+	defer func(fromListener net.Listener) {
+		err := fromListener.Close()
+		if err != nil {
+
+		}
+	}(fromListener)
 
 	for {
 		fromConn, err := fromListener.Accept()
@@ -37,9 +54,9 @@ func main() {
 		}
 
 		//这边最好也做个协程，防止阻塞
-		toConn, err := net.Dial("tcp", *toAddress)
+		toConn, err := net.Dial("tcp", toAddress)
 		if err != nil {
-			fmt.Print(red("can not connect to %s\n", *toAddress))
+			fmt.Print(red("can not connect to %s\n", toAddress))
 			continue
 		}
 
